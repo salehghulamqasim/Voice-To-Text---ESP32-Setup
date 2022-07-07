cbc41a8577bb7ba4198e7831b34cd34c82d8da43
Saleh <100453330+salehghulamqasim@users.noreply.github.com>
2022-07-07 04:50:30 +0300

    Add files via upload

---
+++ task1/scriptFile.js
@@ -0,0 +1,46 @@
+﻿English.addEventListener('click', function () {
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang ='en-US'
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+        document.getElementById("convert_text").innerHTML = transcript;
+        console.log(transcript);
+    });
+
+    if (speech == true) {
+        recognition.start();
+    }
+})
+
+Arabic.addEventListener('click', function () {
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = 'ar-SA'
+
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+        document.getElementById("convert_text").innerHTML = transcript;
+        console.log(transcript);
+    });
+
+    if (speech == true) {
+        recognition.start();
+    }
+})
